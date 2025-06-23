bb9435a7b1bcc0cb0bc3e663cc0e24995237643e
krismeswara <krismeswara86@gmail.com>
2025-06-23 13:17:20 +0700

    fix: replace incorrect test folder with correct test script

--- test/demo.js
+++ test/demo.js
@@ -2,7 +2,7 @@ const { Builder, By, until } = require('selenium-webdriver');
 const assert = require('assert');
 
 describe('Saucedemo - Sorting Z to A', function () {
-  this.timeout(30000);
+  this.timeout(30000); // Set timeout agar test tidak gagal karena lambat
   let driver;
 
   const expectedProductOrder = [
@@ -14,42 +14,44 @@ describe('Saucedemo - Sorting Z to A', function () {
     'Sauce Labs Backpack'
   ];
 
+  //Hook before
   before(async () => {
-    driver = await new Builder().forBrowser('firefox').build();
+    driver = await new Builder().forBrowser('chrome').build();
+    console.log('Browser sudah dibuka');
   });
 
-  after(async () => {
-    await driver.quit();
-  });
-
-  it('Login dan sort Z to A', async () => {
+  it('Login dan sort produk dari Z ke A', async () => {
+    //Login
     await driver.get('https://www.saucedemo.com/');
     await driver.findElement(By.id('user-name')).sendKeys('standard_user');
     await driver.findElement(By.id('password')).sendKeys('secret_sauce');
     await driver.findElement(By.id('login-button')).click();
 
-    // Tunggu halaman produk muncul
+    // Tunggu sampai dropdown sort muncul
     await driver.wait(until.elementLocated(By.className('product_sort_container')), 5000);
 
-    // Klik dropdown lalu pilih "Name (Z to A)"
+    // Klik dropdown & pilih opsi "Name (Z to A)"
     const dropdown = await driver.findElement(By.className('product_sort_container'));
-    await dropdown.sendKeys('Name (Z to A)');
+    await dropdown.click();
+    const zToAOption = await driver.findElement(By.xpath("//option[text()='Name (Z to A)']"));
+    await zToAOption.click();
 
-    // Tunggu produk selesai reload
+    //Tunggu hasil sorting muncul
     await driver.sleep(1500);
 
-    // Ambil semua produk setelah sorting
+    //Ambil nama produk yang muncul
     const productElements = await driver.findElements(By.className('inventory_item_name'));
     const actualProductNames = [];
 
     for (let el of productElements) {
-      const name = await el.getText();
-      actualProductNames.push(name);
+      actualProductNames.push(await el.getText());
     }
 
     console.log('Produk tampil:', actualProductNames);
-
-   
     assert.deepStrictEqual(actualProductNames, expectedProductOrder);
+
+    //Tutup browser secara manual karena tidak pakai hook after()
+    await driver.quit();
+    console.log('Browser closed secara manual di dalam it()');
   });
 });
